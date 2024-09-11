d3fa0df5c3db0a2e8e93e996475fe4b926450303
Candice Cirbo <thecirbos@icloud.com>
2024-09-11 17:44:08 -0600

    Feat: adds the serializer for merchant, added initial test for index route and added merchant index controller

---
+++ app/controllers/api/v1/merchants_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::MerchantsController < ApplicationController
+    def index
+        if params[:count]
+            merchants = Merchant.calculated_count
+        elsif params[:sorted]
+            merchants = Merchant.sort_by_age
+        elsif params[:status]
+            merchants = Merchant.returned_items(params[:status])
+        else 
+            merchants = Merchant.all
+        end
+       render json: MerchantSerializer.new(merchants)
+    end
+
+    # def show
+    #     render json: SongSerializer.format_song(Song.find(params[:id]))
+    #  end
+end
