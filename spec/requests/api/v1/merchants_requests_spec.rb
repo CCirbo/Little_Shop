b3926ac0be2f0e0753d70411722aab47115730a9
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-09-11 20:06:37 -0600

    Show retrieve all merchants test pass

--- spec/requests/api/v1/merchants_requests_spec.rb
+++ spec/requests/api/v1/merchants_requests_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
 
-RSpec.configure do |config| 
-  config.formatter = :documentation 
-end
-
 RSpec.describe "Merchants endpoints" do
-  it "can send a list of merchants" do
-    merchant1 = Merchant.create!(name: "Brown and Sons")
-    merchant2 = Merchant.create!(name: "Brown and Moms")
-    merchant3 = Merchant.create!(name: "Brown and Dads")
+  before(:each) do
+    @merchant_1 = Merchant.create!(name: "Brown and Sons")
+    @merchant_2 = Merchant.create!(name: "Brown and Moms")
+    @merchant_3 = Merchant.create!(name: "Brown and Dads")
+  end
 
+  it "can retrieve ALL merchants" do
+    
     get "/api/v1/merchants"
     merchants = JSON.parse(response.body, symbolize_names: true)[:data]
     
     expect(response).to be_successful
+    expect(merchants.count).to eq(3)
 
     merchants.each do |merchant|
 
       expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(String)
+      expect(merchant[:id]).to be_a(String)
 
       expect(merchant).to have_key(:type)
       expect(merchant[:type]).to be_a(String)
@@ -28,19 +28,20 @@ RSpec.describe "Merchants endpoints" do
       
       expect(attributes).to have_key(:name)
       expect(attributes[:name]).to be_a(String)
+
+      expect(@merchant_1.name).to eq("Brown and Sons")
+      expect(@merchant_2.name).to eq("Brown and Moms")
+      expect(@merchant_3.name).to eq("Brown and Dads")
     end
   end
 
-  # it "can return the count" do
-  #   merchant1 = Merchant.create!(name: "Brown and Sons")
-  #   merchant2 = Merchant.create!(name: "Brown and Moms")
-  #   merchant3 = Merchant.create!(name: "Brown and Dads")
+  xit "can return the count" do
 
-  #   get "/api/v1/merchants?sorted=age"
+    get "/api/v1/merchants?sorted=age"
 
-  #   merchants = JSON.parse(response.body, symbolize_names: true)[:data]
+    merchants = JSON.parse(response.body, symbolize_names: true)[:data]
     
-  #   expect(response).to be_successful
-  # end
+    expect(response).to be_successful
+  end
 end
   
