6f91229db4710aeddf7eb9e3c2f613ca30f540af
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-09-11 20:16:32 -0600

    Add sort newest to oldest test

--- spec/requests/api/v1/merchants_requests_spec.rb
+++ spec/requests/api/v1/merchants_requests_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Merchants endpoints" do
   before(:each) do
-    @merchant_1 = Merchant.create!(name: "Brown and Sons")
-    @merchant_2 = Merchant.create!(name: "Brown and Moms")
-    @merchant_3 = Merchant.create!(name: "Brown and Dads")
+    @merchant_1 = Merchant.create!(name: "Brown and Sons", created_at: 3.seconds.ago)
+    @merchant_2 = Merchant.create!(name: "Brown and Moms", created_at: 2.seconds.ago)
+    @merchant_3 = Merchant.create!(name: "Brown and Dads", created_at: 1.seconds.ago)
   end
 
   it "can retrieve ALL merchants" do
@@ -35,13 +35,35 @@ RSpec.describe "Merchants endpoints" do
     end
   end
 
+  
+  # get all merchants sorted by newest to oldest
+  it "sorts merchants by creation date" do
+    
+    get "/api/v1/posters?sort=desc"
+    posters = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(response).to be_successful
+    expect(posters[:data].count).to eq(3)
+
+    expect(@merchant_3[:data][0][:attributes][:name]).to eq("Brown and Dads")
+    expect(@merchant_2[:data][1][:attributes][:name]).to eq("Brown and Moms")
+    expect(@merchant_1[:data][2][:attributes][:name]).to eq("Brown and Sons")
+  end
+  
+  
+  # get all merchants with calculated count of items
   xit "can return the count" do
-
+    
     get "/api/v1/merchants?sorted=age"
-
+    
     merchants = JSON.parse(response.body, symbolize_names: true)[:data]
     
     expect(response).to be_successful
   end
+
+  
+  # get all merchants with returned items (check invoice)
+
+
 end
   
