d0da72f0dd6bfa1b637ae88b9a5d9f658de2d35e
Jim Macur <jim.macur@gmail.com>
2024-09-12 19:30:15 -0600

    feat: adds a create new item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -4,4 +4,15 @@ class Api::V1::ItemsController < ApplicationController
     items = Item.all
     render json: ItemSerializer.new(items)
   end
+
+  def create
+    new_item = Item.create(item_params)
+    render json: ItemSerializer.new(new_item), status: 201
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+  end
 end
--- spec/requests/api/v1/items_requests_spec.rb
+++ spec/requests/api/v1/items_requests_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.configure do |config| 
- config.formatter = :documentation 
- end
+  config.formatter = :documentation 
+end
 
- RSpec.describe "Items endpoints" do
+RSpec.describe "Items endpoints" do
   it "can send a list of items" do
     Merchant.create!(id: 1, name: "Test Merchant")
     Merchant.create!(id: 2, name: "Test Merchant")
@@ -42,7 +42,56 @@ RSpec.configure do |config|
 
       expect(attributes).to have_key(:merchant_id)
       expect(attributes[:merchant_id]).to be_a(Integer)
-
     end
+  end
+
+  it 'can create a new item' do
+    item_params = {
+    name: "New Item",
+    description: "A cool new item",
+    unit_price: 49.99,
+    merchant_id: 1
+    }
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    Merchant.create!(id: 1, name: "Test Merchant")
+
+    post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
+
+    puts response.status
+    puts response.body
+
+    created_item = Item.last
+
+    expect(response).to be_successful
+
+    expect(created_item.name).to eq(item_params[:name])
+    expect(created_item.description).to eq(item_params[:description])
+    expect(created_item.unit_price).to eq(item_params[:unit_price])
+    expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+
+    item = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(item).to have_key(:id)
+    expect(item[:id]).to be_an(String)
+
+    expect(item).to have_key(:type)
+    expect(item[:type]).to be_a(String)
+
+    expect(item).to have_key(:attributes)
+    attributes = item[:attributes]
+    
+    expect(attributes).to have_key(:name)
+    expect(attributes[:name]).to be_a(String)
+
+    expect(attributes).to have_key(:description)
+    expect(attributes[:description]).to be_a(String)
+
+    expect(attributes).to have_key(:unit_price)
+    expect(attributes[:unit_price]).to be_a(Float)
+
+    expect(attributes).to have_key(:merchant_id)
+    expect(attributes[:merchant_id]).to be_a(Integer)
   end
 end
