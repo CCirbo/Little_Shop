e2786d8c922a04f0189d767204a6195f5ab06799
Jim Macur <jim.macur@gmail.com>
2024-09-14 11:07:55 -0600

    test: adds test for when no invoices are foind with status

--- spec/requests/api/v1/merchant_invoices_request_spec.rb
+++ spec/requests/api/v1/merchant_invoices_request_spec.rb
@@ -1,23 +1,46 @@
 require 'rails_helper'
 
-describe 'GET all invoices for mercahnt based on status' do
-  describe 'GET /api/v1/merchants/:merchant_id/invoices' do
-    it 'returns all invoices for a merchant with a given status' do
-      merchant = create(:merchant)
-      shipped_invoice = create(:invoice, merchant: merchant, status: 'shipped')
-      packaged_invoice = create(:invoice, merchant: merchant, status: 'packaged')
-      create(:invoice, merchant: merchant, status: 'returned')
+it "can send a list of invoices for a merchant based on status" do
+  merchant = Merchant.create!(name: "Test Merchant")
+
+  Invoice.create!(status: "shipped", merchant_id: merchant.id)
+  Invoice.create!(status: "packaged", merchant_id: merchant.id)
+  Invoice.create!(status: "returned", merchant_id: merchant.id)
+
+  get "/api/v1/merchants/#{merchant.id}/invoices?status=shipped"
   
-      get "/api/v1/merchants/#{merchant.id}/invoices?status=shipped"
-  
-      expect(response).to be_successful
-      invoices = JSON.parse(response.body, symbolize_names: true)[:data]
-  
-      expect(invoices).to be_an(Array)
-      expect(invoices.length).to eq(1)
-      expect(invoices.first[:attributes][:status]).to eq('shipped')
-    end
+  invoices = JSON.parse(response.body, symbolize_names: true)[:data]
+  expect(response).to be_successful
+
+  invoices.each do |invoice|
+    expect(invoice).to have_key(:id)
+    expect(invoice[:id]).to be_an(String)
+
+    expect(invoice).to have_key(:type)
+    expect(invoice[:type]).to be_a(String)
+
+    expect(invoice).to have_key(:attributes)
+    attributes = invoice[:attributes]
+    
+    expect(attributes).to have_key(:status)
+    expect(attributes[:status]).to be_a(String)
+    expect(attributes[:status]).to eq("shipped") 
+
+    expect(attributes).to have_key(:merchant_id)
+    expect(attributes[:merchant_id]).to be_a(Integer)
+    expect(attributes[:merchant_id]).to eq(merchant.id)
   end
+
+  it 'returns an empty array if no invoices with the given status are found' do
+    merchant = create(:merchant)
+
+    get "/api/v1/merchants/#{merchant.id}/invoices?status=shipped"
+
+    expect(response).to be_successful
+    invoices = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(invoices).to eq([])
+  end
+
+  
 end
-
-
