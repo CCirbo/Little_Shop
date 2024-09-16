0cca70341a7db7a663aad0f15157686453b15901
Candice Cirbo <thecirbos@icloud.com>
2024-09-15 20:13:06 -0600

    Test: initial tests for model items and refactored for instance variables

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,16 +7,44 @@ RSpec.describe Item, type: :model do
   end
 
   describe '#class methods' do 
-    it 'returns all items by price(Low to High)' do 
-      Merchant.create!(id: 1, name: "Test Merchant")
-      item_1 = Item.create!(name: "Mouse", description: "Clicks", unit_price:100.99 , merchant_id:1)
-      item_2 = Item.create!(name: "Keyboard", description: "Types", unit_price:10.99 , merchant_id:1)
-      item_3 = Item.create!(name: "Pad", description: "Soft", unit_price:120.99 , merchant_id:1)
-      item_4 = Item.create!(name: "Notebook", description: "Gets Written On", unit_price:121.99 , merchant_id:1)
-   
-      expected = [item_2, item_1, item_3, item_4]
+    before(:each) do
+      @merchant = Merchant.create!(name: "Test Merchant")
+      @item_1 = Item.create!(name: "Mouse", description: "Clicks", unit_price: 100.99, merchant: @merchant)
+      @item_2 = Item.create!(name: "Keyboard", description: "Types", unit_price: 10.99, merchant: @merchant)
+      @item_3 = Item.create!(name: "Pad", description: "Soft", unit_price: 120.99, merchant: @merchant)
+      @item_4 = Item.create!(name: "Notebook", description: "Gets Written On", unit_price: 121.99, merchant: @merchant)
+    end
 
+    it 'returns all items sorted by price (Low to High)' do 
+      expected = [@item_2, @item_1, @item_3, @item_4]
       expect(Item.sorted_by_price).to eq(expected)
+    end
+
+    it 'filters items by name' do
+      expect(Item.filter_by_name("Mouse")).to eq([@item_1])
+    end
+
+    it 'filters items by min_price' do
+      expect(Item.filter_by_min(100)).to eq([@item_1, @item_3, @item_4])
+    end
+
+    it 'filters items by max_price' do
+      expect(Item.filter_by_max(100)).to eq([@item_2])
+    end
+
+    it 'raises an error when both name and price parameters are passed' do
+      params = { name: "Mouse", min_price: 50 }
+      expect { Item.sort_and_filter(params) }.to raise_error(ArgumentError, "Cannot search by both name and price")
+    end
+
+    it 'raises an error when min_price is negative' do
+      params = { min_price: -10 }
+      expect { Item.sort_and_filter(params) }.to raise_error(ArgumentError, "min_price must be a positive number")
+    end
+
+    it 'raises an error when max_price is negative' do
+      params = { max_price: -5 }
+      expect { Item.sort_and_filter(params) }.to raise_error(ArgumentError, "max_price must be a positive number")
     end
   end
 end
