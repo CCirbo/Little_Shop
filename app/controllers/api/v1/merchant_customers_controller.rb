7f7a1c4c1c6cc2aa5503b5f64e206e33fda318de
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-09-17 19:55:33 -0600

    Show merchant_customers_controller.rb GOOD

--- app/controllers/api/v1/merchant_customers_controller.rb
+++ app/controllers/api/v1/merchant_customers_controller.rb
@@ -1,24 +1,17 @@
 class Api::V1::MerchantCustomersController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
 
+  def index
+      merchant = Merchant.find(params[:merchant_id])
+      customers = Customer.for_merchant(params[:merchant_id])
+      
+      render json: CustomerSerializer.new(customers)
+  end
 
-    def index
-        merchant = Merchant.find(params[:merchant_id])
-        customers = Customer.for_merchant(params[:merchant_id])
-        
-        render json: CustomerSerializer.new(customers)
-    end
-    
-    #   merchant = Merchant.find(params[:merchant_id])
-    #   customers = Customer.joins(:invoices).where(invoices: { merchant_id: merchant.id }).distinct
-  
-    #   render json: CustomerSerializer.new(customers)
-    private 
+  private 
 
-    def not_found_response(e)
-        render json: ErrorSerializer.new(ErrorMessage.new(e.message, 404))
-          .serialize_json, status: :not_found
-    end
-
-    
+  def not_found_response(e)
+      render json: ErrorSerializer.new(ErrorMessage.new(e.message, 404))
+      .serialize_json, status: :not_found
+  end
 end
