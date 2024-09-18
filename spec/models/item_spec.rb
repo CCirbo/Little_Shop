03dd60d4a68105f2215f6847b6ac2bcfff994ce4
Candice Cirbo <thecirbos@icloud.com>
2024-09-17 20:57:31 -0600

    Test: fixes minor item spec file

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -31,29 +31,30 @@ RSpec.describe Item, type: :model do
     it "filters items by max_price" do
       expect(Item.filter_by_max(100)).to eq([@item_2])
     end
+  end
 
-    describe "Find One By Name" do
-      before(:each) do
-        @merchant = Merchant.create!(name: "Test Merchant")
-        @item_1 = Item.create!(name: "Thing", description: "Thingy", unit_price: 1.99, merchant: @merchant)
-        @item_2 = Item.create!(name: "Anything", description: "Anythingy", unit_price: 10.99, merchant: @merchant)
-        @item_3 = Item.create!(name: "Nothing", description: "Nothingy", unit_price: 100.99, merchant: @merchant)
-      end
+  describe "Find One By Name" do
+    before(:each) do
+      @merchant = Merchant.create!(name: "Test Merchant")
+      @item_1 = Item.create!(name: "Thing", description: "Thingy", unit_price: 1.99, merchant: @merchant)
+      @item_2 = Item.create!(name: "Anything", description: "Anythingy", unit_price: 10.99, merchant: @merchant)
+      @item_3 = Item.create!(name: "Nothing", description: "Nothingy", unit_price: 100.99, merchant: @merchant)
+    end
 
-      context "when there are multiple matches" do
-        it "returns the 1st item in A-Z order with a case insensitive search" do
-          result = Item.find_one_by_name("tHIng")
-          expect(result.id).to eq(@item_2.id)
-        end
-      end
-
-      context "when there are no matches" do
-        it "returns nil" do
-          result = Item.find_one_by_name("xxx")
-          expect(result).to be_nil
-        end
+    context "when there are multiple matches" do
+      it "returns the 1st item in A-Z order with a case insensitive search" do
+        result = Item.find_one_by_name("tHIng")
+        expect(result.id).to eq(@item_2.id)
       end
     end
+
+    context "when there are no matches" do
+      it "returns nil" do
+        result = Item.find_one_by_name("xxx")
+        expect(result).to be_nil
+      end
+    end
+  end
 
     it "raises an error when both name and price parameters are passed" do
       params = { name: "Mouse", min_price: 50 }
@@ -70,35 +71,34 @@ RSpec.describe Item, type: :model do
       expect { Item.sort_and_filter(params) }.to raise_error(ArgumentError, "max_price must be a positive number")
     end
 
-    describe "Sort_and_filter" do
-      it "filters by name only" do
-        params = { name: "Mouse" }
-        expected = [@item_1]
-        result = Item.sort_and_filter(params) 
-    
-        expect(result).to eq(expected)
-      end
+  describe "Sort_and_filter" do
+    it "filters by name only" do
+      params = { name: "Mouse" }
+      expected = [@item_1]
+      result = Item.sort_and_filter(params) 
   
-      it 'filters by min_price only' do
-        params = { min_price: 100 }
-        expected = [@item_1, @item_3, @item_4]
-        result = Item.sort_and_filter(params) 
-    
-        expect(result).to eq(expected)
-      end
-    
-      it 'filters by max_price only' do
-        params = { max_price: 100 }
-        expected = [@item_2]
-        result = Item.sort_and_filter(params) 
-    
-        expect(result).to eq(expected)
-      end
-   
-      it "Sad-Path raises an error when both name and price parameters are passed" do
-        params = { name: "Pad", min_price: 50, max_price: 120 }
-        expect { Item.sort_and_filter(params) }.to raise_error(ArgumentError, "Cannot search by both name and price")
-      end
+      expect(result).to eq(expected)
+    end
+
+    it 'filters by min_price only' do
+      params = { min_price: 100 }
+      expected = [@item_1, @item_3, @item_4]
+      result = Item.sort_and_filter(params) 
+  
+      expect(result).to eq(expected)
+    end
+  
+    it 'filters by max_price only' do
+      params = { max_price: 100 }
+      expected = [@item_2]
+      result = Item.sort_and_filter(params) 
+  
+      expect(result).to eq(expected)
+    end
+  
+    it "Sad-Path raises an error when both name and price parameters are passed" do
+      params = { name: "Pad", min_price: 50, max_price: 120 }
+      expect { Item.sort_and_filter(params) }.to raise_error(ArgumentError, "Cannot search by both name and price")
     end
   end
 end
